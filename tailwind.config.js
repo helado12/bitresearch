9d88ac9d57e2d34db3104c4066067d60f080dc85
Aathik Ahamed <aathikahamed@gmail.com>
2022-01-22 21:27:50 +0000

    📦 NEW: created landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        black: "4px 6px 0 0 rgba(0, 0, 0, 1)",
+        "black-lg": "5px 7px 0 0 rgba(0, 0, 0, 1)",
+      },
+    },
   },
   plugins: [],
 };
