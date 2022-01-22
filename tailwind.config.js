fda60bf90f861d1d79fe115ed531e8c76e253e73
Aathik Ahamed <aathikahamed@gmail.com>
2022-01-22 22:37:00 +0000

    📦 NEW: created home page of the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
       boxShadow: {
         black: "4px 6px 0 0 rgba(0, 0, 0, 1)",
         "black-lg": "5px 7px 0 0 rgba(0, 0, 0, 1)",
+        light: "0 4px 14px 0 rgba(0, 0, 0, 0.07)",
       },
     },
   },
